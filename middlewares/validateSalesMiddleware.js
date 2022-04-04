9583fc076184a1c3cc660ee41170f3fe54fd5b93
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-04-04 02:31:50 -0300

    feat(correçoes): correçoes no codigo

--- middlewares/validateSalesMiddleware.js
+++ middlewares/validateSalesMiddleware.js
@@ -1,12 +1,10 @@
 const error = (status, message) => ({ status, message });
 
 const validateProductId = (productId) => {
-  console.log(productId);
   if (!productId) throw error(400, '"productId" is required');
 };
 
 const validateQuantity = (quantityOfSale) => {
-  console.log(quantityOfSale);
   if (quantityOfSale === undefined) throw error(400, '"quantity" is required');
   if (Number(quantityOfSale) <= 0) {
     throw error(422, '"quantity" must be greater than or equal to 1');
