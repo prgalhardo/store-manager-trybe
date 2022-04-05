4d45a4a040f7994b597acdaf99891f90fa31d73f
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-04-05 02:36:24 -0300

    feat(teste): salesService teste

---
+++ test/unit/services/salesServices.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const sinon = require('sinon');
+const connection = require('../../../config/connection');
+const { expect } = require('chai');
+const SalesService = require('../../../services/salesService');
+const salesMock = require('../mocks/salesMock');
+
+describe('SalesService', () => {
+
+  describe('getAll', () => {
+
+    describe('Quando o conteúdo de vendas retornar vazio', () => {
+      before(() => {
+        sinon.stub(connection, 'execute').resolves([salesMock.empty]);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+      it('retorno será vazio', async () => {
+        const sales = await SalesService.getAll();
+        expect(sales).to.be.deep.eq(salesMock.empty);
+      });
+    });
+    describe('Quando o conteúdo de vendas retornar conforme o esperado', () => {
+      before(() => {
+        sinon.stub(connection, 'execute').resolves([salesMock.full]);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+      it('retorno será um array de objetos', async () => {
+        const sales = await SalesService.getAll();
+        expect(sales).to.be.deep.eq(salesMock.full);
+      });
+    });
+  });
+});
