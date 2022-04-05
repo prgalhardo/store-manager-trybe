207845d552227ed0fc615fd2859258f9daec9548
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-04-05 01:31:26 -0300

    feat(teste): productsController, findById teste

--- test/unit/controllers/productsControllers.js
+++ test/unit/controllers/productsControllers.js
@@ -22,10 +22,6 @@ describe('ProductsController', () => {
       after(() => {
         ProductsService.getAll.restore();
       });
-      it('deve chamar a função `res.status` com o valor 200', async () => {
-        await ProductsController.getAll(req, res);
-        expect(res.status.calledWith(200)).to.be.true;
-      });
       it('deve chamar a função `res.json` com uma array vazia', async () => {
         await ProductsController.getAll(req, res);
         expect(res.json.calledWith(productsMock.empty)).to.be.true;
@@ -48,9 +44,71 @@ describe('ProductsController', () => {
         await ProductsController.getAll(req, res);
         expect(res.status.calledWith(200)).to.be.true;
       });
-      it('deve chamar a função `res.json` com uma array de objetos', async () => {
+      it('deve chamar a função `res.json` com um objeto', async () => {
         await ProductsController.getAll(req, res);
         expect(res.json.calledWith(productsMock.full)).to.be.true;
+      });
+    });
+  });
+
+  describe('findById', () => {
+
+    describe('Quando for feita a procura por um id que não existe', () => {
+      const req = {};
+      const res = {};
+
+      req.params = {
+        "id": 5,
+      };
+
+      before(() => {
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub();
+
+        sinon.stub(ProductsService, 'findById').resolves();
+      });
+      after(() => {
+        ProductsService.findById.restore();
+      });
+      it('deve chamar a função `res.status` com o valor 404', async () => {
+        await ProductsController.findById(req, res);
+        expect(res.status.calledWith(404)).to.be.true;
+      });
+      it('deve chamar a função `res.json` com uma mensagem de erro', async () => {
+        await ProductsController.findById(req, res);
+        expect(res.json.calledWith({ message: "Product not found" })).to.be.true;
+      });
+    });
+    describe('Quando for feita a procura por um id que existe', () => {
+      const req = {};
+      const res = {};
+
+      req.params = {
+        "id": 1,
+      };
+
+      const product = {
+        "id": 1,
+        "name": "produto A",
+        "quantity": 10
+      };
+
+      before(() => {
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub();
+
+        sinon.stub(ProductsService, 'findById').resolves(product);
+      });
+      after(() =>{
+        ProductsService.findById.restore();
+      });
+      it('deve chamar a função `res.status`com o valor 200', async () => {
+        await ProductsController.findById(req, res);
+        expect(res.status.calledWith(200)).to.be.true;
+      });
+      it('deve chamar a função `res.json` com o objeto do produto', async () => {
+        await ProductsController.findById(req, res);
+        expect(res.json.calledWith(product)).to.be.true;
       });
     });
   });
